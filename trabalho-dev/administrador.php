0d12201e3ccd857fd0f27d232992d6481b34b79f
Pedro Sperotto <114098634+Lecrepus17@users.noreply.github.com>
2023-04-06 10:06:16 -0300

    aas

--- trabalho-dev/administrador.php
+++ trabalho-dev/administrador.php
@@ -1,6 +1,14 @@
 <?php
     require('twig_carregar.php');
+    require('pdo.inc.php');
 
+    $sql = $conex->query('SELECT * FROM alunos');
+    $sql = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+    echo $twig->render('administrador/index.html', [
+          'user' => $sql,
+     
+          ]);
 	
-    var_dump($twig->render('administrador/index.php')) ;
+  
 
